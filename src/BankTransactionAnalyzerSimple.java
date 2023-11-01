c1a7d208d33a89b48bfec92076ccc02626e450ac
Gustavo Iosimura <guiosimura@hotmail.com>
2023-11-01 08:08:09 -0300

    first steps -> reads a file named bank_data.csv

---
+++ src/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankTransactionAnalyzerSimple {
+
+    private static final String RESOURCES = "src/bank_data.csv";
+
+    public static void main(String[] args) throws IOException {
+
+        final Path path = Paths.get(RESOURCES);
+        final List<String> lines = Files.readAllLines(path);
+
+        double total = 0d;
+
+        for (String line : lines) {
+
+            String[] columns = line.split(",");
+ 
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount; 
+            
+        }
+
+        System.out.println("The total for all transactions is " + total);
+    }
+}
